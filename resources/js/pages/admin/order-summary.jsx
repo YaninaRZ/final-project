85e6cd6631ee9f01dd75fd1be52c2765770fdddc
Yanina <yanina.razvetskaya@laplateforme.io>
2025-09-15 14:04:49 +0200

    delete selected product admin

--- resources/js/pages/admin/order-summary.jsx
+++ resources/js/pages/admin/order-summary.jsx
@@ -11,9 +11,6 @@ export default function OrderSummary({ order, amount }) {
     return (
         <AdminLayout>
             <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow-sm">
-                <div className="px-4 py-5 sm:px-6">
-                    <h2 className="sr-only">Products</h2>
-                </div>
                 <div className="px-4 py-5 sm:p-6">
                     <p>Details of the command #{order.id}</p>
                 </div>
